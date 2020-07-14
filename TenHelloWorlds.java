c7920c5fb4b8cb15feaf2a3eac901a86f63dcb5e
rajends1 <surajemail@yahoo.com>
2020-07-14 17:18:35 +0100

    First Commit

---
+++ TenHelloWorlds.java
@@ -0,0 +1,36 @@
+/******************************************************************************
+ *  Compilation:  javac TenHelloWorlds.java
+ *  Execution:    java TenHelloWorlds
+ *
+ *  Prints "Hello, World" ten times.
+ *
+ *  % java TenHelloWorlds
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *
+ ******************************************************************************/
+
+public class TenHelloWorlds {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+
+}
